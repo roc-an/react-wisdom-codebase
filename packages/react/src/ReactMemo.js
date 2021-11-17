4e1578f2a02a483daec2fdae8e6d9498ec5f86a6
AnHongpeng <rocanhongpeng@163.com>
2021-11-17 16:05:03 +0800

    feat: 注解 ReactMemo.js

--- packages/react/src/ReactMemo.js
+++ packages/react/src/ReactMemo.js
@@ -9,8 +9,12 @@ import {REACT_MEMO_TYPE} from 'shared/ReactSymbols';
 
 import isValidElementType from 'shared/isValidElementType';
 
+// React.memo API Doc: https://reactjs.org/docs/react-api.html#reactmemo
+// React.memo 是个高阶组件，传入一个函数组件，得到经过优化后的新组件
+// 如果新旧 props 未改变（浅比较），将跳过渲染并直接复用最近一次的渲染结果
 export function memo<Props>(
   type: React$ElementType,
+  // 可选的自定义 areEqual 函数，根据新旧 props 判断，return true 表示没变化，将复用上次渲染结果
   compare?: (oldProps: Props, newProps: Props) => boolean,
 ) {
   if (__DEV__) {
